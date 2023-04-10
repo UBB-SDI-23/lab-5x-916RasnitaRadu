82660eb3ccbb8029f90ed9d23ae06152d2ddfffe
RaduRzn <91827415+916RasnitaRadu@users.noreply.github.com>
2023-04-10 16:30:36 +0300

    Update A2Application.java

--- src/main/java/com/example/A2/A2Application.java
+++ src/main/java/com/example/A2/A2Application.java
@@ -27,7 +27,7 @@ public class A2Application {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://inquisitive-cuchufli-ecb700.netlify.app/"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://64340f07f7271a1c7bcfd165--storied-cranachan-5fead9.netlify.app/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
